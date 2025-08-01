851e048f600e6ecffb50d6b10f4a77cc03bef9ce
biohack5079 <biohack5079@gmail.com>
2025-08-02 05:17:03 +0900

    changed

--- Flask/app.py
+++ Flask/app.py
@@ -2,15 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route(apigesture, methods=[POST])
-def handle_gesture()
+@app.route('/apigesture', methods=['POST'])
+def handle_gesture():
     data = request.get_json()
-    print(Received gesture, data)
-    return jsonify({status ok, message Gesture received, echo data})
+    print("Received gesture:", data)
+    return jsonify({"status": "ok", "message": "Gesture received", "echo": data})
 
-@app.route()
-def index()
-    return G1m API Ready
+@app.route('/')
+def index():
+    return "G1m API Ready"
 
-if __name__ == __main__
+if __name__ == '__main__':
     app.run(debug=True)
