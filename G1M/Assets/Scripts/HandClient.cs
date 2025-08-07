ca05e6fb70f4964a5f8a6ce9f27c7d8fb150c744
biohack5079 <biohack5079@gmail.com>
2025-08-07 23:19:38 +0900

    changed

--- G1M/Assets/Scripts/HandClient.cs
+++ G1M/Assets/Scripts/HandClient.cs
@@ -99,7 +99,11 @@ public class HandClient : MonoBehaviour
         {
             iceServers = new RTCIceServer[]
             {
-                new RTCIceServer { urls = new string[] { "stun:stun.l.google.com:19302" } }
+                new RTCIceServer { urls = new string[] { "stun:stun.l.google.com:19302" } },
+                new RTCIceServer { urls = new string[] { "stun:stun1.l.google.com:19302" } },
+                new RTCIceServer { urls = new string[] { "stun:stun2.l.google.com:19302" } },
+                new RTCIceServer { urls = new string[] { "stun:stun.services.mozilla.com:3478" } },
+                new RTCIceServer { urls = new string[] { "stun:stun.voip.blackberry.com:3478" } }                
             }
         };
         _peerConnection = new RTCPeerConnection(ref configuration);
